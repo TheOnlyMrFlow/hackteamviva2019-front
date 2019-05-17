daaab85d06a7765f4870655377a8143894b376ec
TheOnlyMrFlow <comte.florian@gmail.com>
2019-05-17 15:06:01 +0200

    2 routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+// import Hello from '@/components/Hello'
+import Home from '@/components/patient/Home'
+import Dashboard from '@/components/physician/Dashboard'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Hello',
+    //   component: Hello
+    // },
     {
-      path: '/',
-      name: 'Hello',
-      component: Hello
+      path: '/patient',
+      name: 'Patient',
+      component: Home
+    },
+    {
+      path: '/physician',
+      name: 'Physician',
+      component: Dashboard
     }
   ]
 })
